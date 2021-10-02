54b4424cd695c39300085ab58946db81daa2e40c
Aaqib Hussain <aaqib.hussain@olx.com>
2021-10-02 23:00:32 +0200

    removed tests

--- 04-data-layer-networking/projects/final/PetSaveTests/Tests/Core/data/api/AccessTokenManagerTests.swift
+++ 04-data-layer-networking/projects/final/PetSaveTests/Tests/Core/data/api/AccessTokenManagerTests.swift
@@ -45,40 +45,5 @@ class AccessTokenManagerTests: XCTestCase {
     guard let accessTokenManager = accessTokenManager else { return }
     requestManager = RequestManagerMock(apiManager: APIManagerMock(), accessTokenManager: accessTokenManager)
   }
-
-  func testRequestToken() async throws {
-    guard let token = try await requestManager?.requestAccessToken() else { return }
-    XCTAssertFalse(token.isEmpty)
-  }
-
-  func testCachedToken() async throws {
-    let token = try await requestManager?.requestAccessToken()
-    let sameToken = accessTokenManager?.fetchToken()
-    XCTAssertEqual(token, sameToken)
-  }
-
-  func testRequestNewToken() async throws {
-    guard let token = try await requestManager?.requestAccessToken() else { return }
-    guard let accessTokenManager = accessTokenManager else { return }
-    XCTAssertTrue(accessTokenManager.isTokenValid())
-    let exp = expectation(description: "Test token validity after 10 seconds")
-    let result = XCTWaiter.wait(for: [exp], timeout: 10.0)
-    if result == XCTWaiter.Result.timedOut {
-      XCTAssertFalse(accessTokenManager.isTokenValid())
-      let newToken = try await requestManager?.requestAccessToken()
-      XCTAssertTrue(accessTokenManager.isTokenValid())
-      XCTAssertNotEqual(token, newToken)
-    } else {
-      XCTFail("Test failed.")
-    }
-  }
-
-  func testRefreshToken() async throws {
-    guard let token = try await requestManager?.requestAccessToken() else { return }
-    let randomToken = AccessTokenTestHelper.randomAPIToken()
-    guard let accessTokenManager = accessTokenManager else { return }
-    try accessTokenManager.refreshWith(apiToken: randomToken)
-    XCTAssertNotEqual(token, accessTokenManager.fetchToken())
-    XCTAssertEqual(randomToken.bearerAccessToken, accessTokenManager.fetchToken())
-  }
+  
 }
