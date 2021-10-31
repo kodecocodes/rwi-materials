df183a424396eeca5b7f11cd775d5e018b73a6e2
Aaqib Hussain <aaqib.hussain@olx.com>
2021-10-31 12:52:19 +0100

    fixed some stuff

--- 07-multi-module-app/projects/starter/PetSaveOnboarding/PetSaveOnboarding.h
+++ 07-multi-module-app/projects/starter/PetSaveOnboarding/PetSaveOnboarding.h
@@ -1,15 +1,15 @@
 /// Copyright (c) 2021 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -29,16 +29,15 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-// swiftlint:disable all
 
-import SwiftUI
+#import <Foundation/Foundation.h>
 
-extension Color {
-    static var rwGreen: Color {
-        Color("rw-green", bundle: .module)
-    }
-    
-    static var rwDark: Color {
-        Color("rw-dark", bundle: .module)
-    }
-}
+//! Project version number for PetSaveOnboarding.
+FOUNDATION_EXPORT double PetSaveOnboardingVersionNumber;
+
+//! Project version string for PetSaveOnboarding.
+FOUNDATION_EXPORT const unsigned char PetSaveOnboardingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PetSaveOnboarding/PublicHeader.h>
+
+
