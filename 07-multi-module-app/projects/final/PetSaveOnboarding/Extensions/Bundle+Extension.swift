e9b9ae125e32780f277f3cdf0d64d525214563ae
Aaqib Hussain <iaaqibhussain@outlook.com>
2021-11-13 11:18:29 +0100

    remove disable all from Bundle+Extension.swift

--- 07-multi-module-app/projects/final/PetSaveOnboarding/Extensions/Bundle+Extension.swift
+++ 07-multi-module-app/projects/final/PetSaveOnboarding/Extensions/Bundle+Extension.swift
@@ -29,14 +29,13 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-// swiftlint:disable all
 
 import Foundation
 
 extension Bundle {
-    public static var module: Bundle {
-        Bundle(
-            identifier: "com.raywenderlich.PetSaveOnboarding"
-        )!
-    }
+  public static var module: Bundle? {
+    Bundle(
+      identifier: "com.raywenderlich.PetSaveOnboarding"
+    )
+  }
 }
