19ec96c7f73a7fd4b84e1c8685116a3015ab0ad6
Aaqib Hussain <iaaqibhussain@outlook.com>
2021-11-13 11:12:16 +0100

    remove disable all from pet.swift

--- 07-multi-module-app/projects/final/PetSaveOnboarding/Model/Pet.swift
+++ 07-multi-module-app/projects/final/PetSaveOnboarding/Model/Pet.swift
@@ -29,28 +29,26 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-// swiftlint:disable all
 
 import SwiftUI
 
 struct Pet: Identifiable {
-    let id = UUID()
-    
-    let petImage: Image
-    let position: CGPoint
+  let id = UUID()
+  let petImage: Image
+  let position: CGPoint
 }
 
 extension Pet {
-    static let backgroundPets: [Pet] = {
-        let bounds = UIScreen.main.bounds
-        return [
-            Pet(petImage: .bird, position: .init(x: bounds.minX + 50, y: 20)),
-            Pet(petImage: .catPurple, position: .init(x: bounds.maxX, y: bounds.maxY / 2)),
-            Pet(petImage: .catPurr, position: .init(x: bounds.maxX, y: bounds.maxY - 100)),
-            Pet(petImage: .chameleon, position: .init(x: bounds.minX, y: bounds.maxY / 2)),
-            Pet(petImage: .dogBoneStand, position: .init(x: bounds.minX, y: bounds.maxY / 1.5)),
-            Pet(petImage: .dogBone, position: .init(x: bounds.maxX - 50, y: 50)),
-            Pet(petImage: .dogTennisBall, position: .init(x: bounds.minX, y: bounds.maxY - 10)),
-        ]
-    }()
+  static let backgroundPets: [Pet] = {
+    let bounds = UIScreen.main.bounds
+    return [
+      Pet(petImage: .bird, position: .init(x: bounds.minX + 50, y: 20)),
+      Pet(petImage: .catPurple, position: .init(x: bounds.maxX, y: bounds.maxY / 2)),
+      Pet(petImage: .catPurr, position: .init(x: bounds.maxX, y: bounds.maxY - 100)),
+      Pet(petImage: .chameleon, position: .init(x: bounds.minX, y: bounds.maxY / 2)),
+      Pet(petImage: .dogBoneStand, position: .init(x: bounds.minX, y: bounds.maxY / 1.5)),
+      Pet(petImage: .dogBone, position: .init(x: bounds.maxX - 50, y: 50)),
+      Pet(petImage: .dogTennisBall, position: .init(x: bounds.minX, y: bounds.maxY - 10))
+    ]
+  }()
 }
