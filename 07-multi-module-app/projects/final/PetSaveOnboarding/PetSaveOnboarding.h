5f1cf6295237ecbf2c9002aaededaf84b88092fe
Pinal Naik <pinal.naik@gmail.com>
2021-11-25 11:57:23 +0530

    Not needed comments removed from one file.

--- 07-multi-module-app/projects/final/PetSaveOnboarding/PetSaveOnboarding.h
+++ 07-multi-module-app/projects/final/PetSaveOnboarding/PetSaveOnboarding.h
@@ -32,12 +32,6 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for PetSaveOnboarding.
 FOUNDATION_EXPORT double PetSaveOnboardingVersionNumber;
 
-//! Project version string for PetSaveOnboarding.
 FOUNDATION_EXPORT const unsigned char PetSaveOnboardingVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <PetSaveOnboarding/PublicHeader.h>
-
-
