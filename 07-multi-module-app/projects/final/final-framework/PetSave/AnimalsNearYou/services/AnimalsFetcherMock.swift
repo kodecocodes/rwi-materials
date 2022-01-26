d83b6e467439f7b1be9e687205663fc037956690
Aaqib Hussain <iaaqibhussain@outlook.com>
2022-01-26 22:43:17 +0100

    updating final chapter for framework

--- 07-multi-module-app/projects/final/final-framework/PetSave/AnimalsNearYou/services/AnimalsFetcherMock.swift
+++ 07-multi-module-app/projects/final/final-framework/PetSave/AnimalsNearYou/services/AnimalsFetcherMock.swift
@@ -30,7 +30,7 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import CoreLocation
+import Foundation
 
 struct AnimalsFetcherMock: AnimalsFetcher {
   func fetchAnimals(page: Int) async -> [Animal] {
