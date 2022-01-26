ddcd9b3cf2f45ab8a7b40cb3fac88218b8347526
Renan Dias <renanbenattidias@gmail.com>
2022-01-26 11:55:00 -0400

    Sync

--- 04-data-layer-networking/projects/starter/PetSave/AnimalsNearYou/views/AnimalRow.swift
+++ 04-data-layer-networking/projects/starter/PetSave/AnimalsNearYou/views/AnimalRow.swift
@@ -37,24 +37,23 @@ struct AnimalRow: View {
 
   var body: some View {
     HStack {
-      AsyncImage(
-        url: animal.picture,
-        content: { image in image
+      AsyncImage(url: animal.picture) { image in
+        image
           .resizable()
-        }, placeholder: {
-          Image("rw-logo")
-            .resizable()
-            .overlay {
-              if animal.picture != nil {
-                ProgressView()
-                  .frame(maxWidth: .infinity, maxHeight: .infinity)
-                  .background(.gray.opacity(0.4))
-              }
+      } placeholder: {
+        Image("rw-logo")
+          .resizable()
+          .overlay {
+            if animal.picture != nil {
+              ProgressView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(.gray.opacity(0.4))
             }
-        })
-        .aspectRatio(contentMode: .fit)
-        .frame(width: 112, height: 112)
-        .cornerRadius(8)
+          }
+      }
+      .aspectRatio(contentMode: .fit)
+      .frame(width: 112, height: 112)
+      .cornerRadius(8)
 
       VStack(alignment: .leading) {
         Text(animal.name)
