f3f5bc4e9d8f321b855ba953268ed772650a2670
Libranner <libranner@gmail.com>
2022-03-15 00:28:19 +0100

    FPE: Fix warnings

--- 03-data-layer-networking/projects/challenge/PetSaveTests/Tests/Core/data/api/AccessTokenManagerTests.swift
+++ 03-data-layer-networking/projects/challenge/PetSaveTests/Tests/Core/data/api/AccessTokenManagerTests.swift
@@ -38,44 +38,43 @@ class AccessTokenManagerTests: XCTestCase {
   let token = AccessTokenTestHelper.randomAPIToken()
   override func setUp() {
     super.setUp()
-    
     guard let userDefaults = UserDefaults(suiteName: #file) else {
       XCTFail("Unable to instantiate user defaults")
       return
     }
-    
+
     userDefaults.removePersistentDomain(forName: #file)
     userDefaults.set(token.expiresAt.timeIntervalSince1970, forKey: AppUserDefaultsKeys.expiresAt)
     userDefaults.set(token.bearerAccessToken, forKey: AppUserDefaultsKeys.bearerAccessToken)
-    
+
     accessTokenManager = AccessTokenManager(userDefaults: userDefaults)
   }
-  
+
   func testRequestToken() {
     guard let token = accessTokenManager?.fetchToken() else {
       XCTFail("Didn't get token from the access token manager")
       return
     }
-    
+
     XCTAssertFalse(token.isEmpty)
   }
-  
+
   func testCachedToken() {
     guard let sameToken = accessTokenManager?.fetchToken() else {
       XCTFail("Didn't get token from the access token manager")
       return
     }
-    
+
     XCTAssertEqual(token.bearerAccessToken, sameToken)
   }
-  
+
   func testRefreshToken() throws {
     let randomToken = AccessTokenTestHelper.randomAPIToken()
     guard let accessTokenManager = accessTokenManager else {
       XCTFail("Access token manager object is nil")
       return
     }
-    
+
     try accessTokenManager.refreshWith(apiToken: randomToken)
     XCTAssertNotEqual(token.bearerAccessToken, accessTokenManager.fetchToken())
     XCTAssertEqual(randomToken.bearerAccessToken, accessTokenManager.fetchToken())
