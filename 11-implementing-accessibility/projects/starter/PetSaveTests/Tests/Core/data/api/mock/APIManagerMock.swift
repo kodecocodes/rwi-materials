c5ebabbde4e08f48c6e323b5821aebfa19730ae0
Libranner <libranner@gmail.com>
2022-04-02 01:37:26 +0200

    FPE: Changes from chapter 03

--- 11-implementing-accessibility/projects/starter/PetSaveTests/Tests/Core/data/api/mock/APIManagerMock.swift
+++ 11-implementing-accessibility/projects/starter/PetSaveTests/Tests/Core/data/api/mock/APIManagerMock.swift
@@ -34,7 +34,11 @@ import XCTest
 @testable import PetSave
 
 struct APIManagerMock: APIManagerProtocol {
-  func initRequest(with data: RequestProtocol, authToken: String) async throws -> Data {
-    return try Data(contentsOf: URL(fileURLWithPath: data.path), options: .mappedIfSafe)
+  func perform(_ request: RequestProtocol, authToken: String) async throws -> Data {
+    return try Data(contentsOf: URL(fileURLWithPath: request.path), options: .mappedIfSafe)
+  }
+
+  func requestToken() async throws -> Data {
+    Data(AccessTokenTestHelper.generateValidToken().utf8)
   }
 }
