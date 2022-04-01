88fff11fe2f76e9e06c5a900f078f990c1e62041
Libranner <libranner@gmail.com>
2022-04-02 01:30:17 +0200

    FPE: Changes from chapter 03

--- 10-adding-animations-and-custom-controls/projects/starter/PetSaveTests/Tests/Core/data/coreData/CoreDataTests.swift
+++ 10-adding-animations-and-custom-controls/projects/starter/PetSaveTests/Tests/Core/data/coreData/CoreDataTests.swift
@@ -52,7 +52,7 @@ class CoreDataTests: XCTestCase {
       let first = results.first else { return }
 
       XCTAssert(first.name == "CHARLA", """
-        Pet name did not match, was expecting Kiki, got
+        Pet name did not match, was expecting CHARLA, got
         \(String(describing: first.name))
       """)
       XCTAssert(first.type == "Dog", """
@@ -73,13 +73,14 @@ class CoreDataTests: XCTestCase {
     guard let results = try? previewContext.fetch(fetchRequest),
       let first = results.first else { return }
 
+    let expectedResult = results.count - 1
     previewContext.delete(first)
 
-    guard let results = try? previewContext.fetch(fetchRequest)
+    guard let resultsAfterDeletion = try? previewContext.fetch(fetchRequest)
       else { return }
 
-    XCTAssert(results.count == 9, """
-      The number of results was expected to be 9 after deletion, was \(results.count)
+    XCTAssertEqual(expectedResult, resultsAfterDeletion.count, """
+      The number of results was expected to be \(expectedResult) after deletion, was \(results.count)
     """)
   }
 
